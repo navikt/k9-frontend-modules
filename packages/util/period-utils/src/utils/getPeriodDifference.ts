44fa1175e686c9bc6e1543d62fd877fb5d7b6554
Erlend Vige <erlend.edvardsen.vige@nav.no>
2021-04-27 22:00:38 +0200

    getPeriodDifference tests

--- packages/util/period-utils/src/utils/getPeriodDifference.ts
+++ packages/util/period-utils/src/utils/getPeriodDifference.ts
@@ -1,33 +1,21 @@
-import { Dayjs } from 'dayjs';
-import {
-    makeArrayWithoutDuplicates,
-    getArrayDifference,
-} from '@navikt/array-utils';
-import {
-    initializeDate,
-    isSameOrBefore,
-    isDayAfter,
-    dateSorter,
-} from '@navikt/date-utils';
+import { makeArrayWithoutDuplicates, getArrayDifference } from '@navikt/array-utils';
+import { initializeDate, isSameOrBefore, isDayAfter, dateSorter } from '@navikt/date-utils';
 import Period from './../types/Period';
+import { dateStringSorter } from '@navikt/date-utils/src/sort';
 
-export function getPeriodAsListOfDays(period: Period): Dayjs[] {
+export function getPeriodAsListOfDays(period: Period): string[] {
     const fom = initializeDate(period.fom);
     const tom = initializeDate(period.tom);
 
     const list = [];
-    for (
-        let currentDate = fom;
-        isSameOrBefore(currentDate, tom);
-        currentDate = currentDate.add(1, 'day')
-    ) {
-        list.push(currentDate);
+    for (let currentDate = fom; isSameOrBefore(currentDate, tom); currentDate = currentDate.add(1, 'day')) {
+        list.push(currentDate.format('YYYY-MM-DD'));
     }
 
     return list;
 }
 
-export function getPeriodsAsListOfDays(period: Period[]) {
+export function getPeriodsAsListOfDays(period: Period[]): string[] {
     const days = period.map(getPeriodAsListOfDays).flat();
     return makeArrayWithoutDuplicates(days);
 }
@@ -54,9 +42,7 @@ export function convertListOfDaysToPeriods(days: string[]): Period[] {
             currentSplit.push(days[i]);
         } else {
             const currentDay = initializeDate(days[i]);
-            const previousDay = initializeDate(
-                currentSplit[currentSplitCount - 1]
-            );
+            const previousDay = initializeDate(currentSplit[currentSplitCount - 1]);
             if (isDayAfter(previousDay, currentDay)) {
                 currentSplit.push(days[i]);
             } else {
@@ -70,11 +56,9 @@ export function convertListOfDaysToPeriods(days: string[]): Period[] {
     return getDaySequencesAsListOfPeriods(daySplit);
 }
 
-function periodDifference(basePeriods: Period[], periodsToExclude: Period[]) {
-    const baseListOfDays = getPeriodsAsListOfDays(basePeriods).sort(dateSorter);
-    const daysToExclude = getPeriodsAsListOfDays(periodsToExclude).sort(
-        dateSorter
-    );
+function periodDifference(basePeriods: Period[], periodsToExclude: Period[]): Period[] {
+    const baseListOfDays = getPeriodsAsListOfDays(basePeriods).sort(dateStringSorter);
+    const daysToExclude = getPeriodsAsListOfDays(periodsToExclude).sort(dateStringSorter);
     const daysToInclude = getArrayDifference(baseListOfDays, daysToExclude);
     return convertListOfDaysToPeriods(daysToInclude);
 }
