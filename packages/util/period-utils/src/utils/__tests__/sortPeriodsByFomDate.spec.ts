8ad8b44e33344a083738abc9a51f7866aef3da58
Hallvard Andreas Stark <hallvard@scel.to>
2021-04-30 13:55:52 +0200

    la inn visning av testdekning la inn flere tester av Period

--- packages/util/period-utils/src/utils/__tests__/sortPeriodsByFomDate.spec.ts
+++ packages/util/period-utils/src/utils/__tests__/sortPeriodsByFomDate.spec.ts
@@ -7,12 +7,16 @@ describe('sortPeriodsByFomDate', () => {
             new Period('2032-01-10', '2032-01-15'),
             new Period('2032-01-01', '2032-01-05'),
             new Period('2032-01-06', '2032-01-09'),
+            new Period('2032-01-16', '2032-01-20'),
+            new Period('2032-01-16', '2032-01-20'),
         ];
         const result = periods.sort(sortPeriodsByFomDate);
         expect(result).toEqual([
             new Period('2032-01-01', '2032-01-05'),
             new Period('2032-01-06', '2032-01-09'),
             new Period('2032-01-10', '2032-01-15'),
+            new Period('2032-01-16', '2032-01-20'),
+            new Period('2032-01-16', '2032-01-20'),
         ]);
     });
 });
