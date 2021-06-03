13c56f46184cc5afd83f1b89ceb5f98973695d54
Hallvard Andreas Stark <57254397+hallvardastark@users.noreply.github.com>
2021-06-03 15:53:23 +0200

    la inn ikoner fra medisinsk-vilkår (#11)
    * la inn ikoner fra medisinsk-vilkår
    
    * bedre lesbarhet i index.ts

--- packages/ui/react-components/index.ts
+++ packages/ui/react-components/index.ts
@@ -1,3 +1,4 @@
+/** Vanlige react-komponenter **/
 export { default as ContentWithTooltip } from './src/content-with-tooltip/ContentWithTooltip';
 export { default as DetailView, DetailViewProps } from './src/detail-view/DetailView';
 export { default as IndicatorWithOverlay } from './src/indicator-with-overlay/IndicatorWithOverlay';
@@ -15,9 +16,40 @@ export { default as LinkButton } from './src/link-button/LinkButton';
 export { default as TitleWithUnderline } from './src/title-with-underline/TitleWithUnderline';
 export { default as ExpandableLabel } from './src/expandable-label/ExpandableLabel';
 export { default as FieldError } from './src/field-error/FieldError';
+
+/** react-hook-form-komponenter **/
 export { default as CheckboxGroup } from './src/form/wrappers/CheckboxGroup';
 export { default as Datepicker } from './src/form/wrappers/Datepicker';
 export { default as PeriodpickerList } from './src/form/wrappers/PeriodpickerList';
 export { default as RadioGroupPanel } from './src/form/wrappers/RadioGroupPanel';
 export { default as TextArea } from './src/form/wrappers/TextArea';
 export { default as YesOrNoQuestion } from './src/form/wrappers/YesOrNoQuestion';
+
+/** Ikoner **/
+export { default as ArrowIcon } from './src/icons/ArrowIcon';
+export { default as BucketIcon } from './src/icons/BucketIcon';
+export { default as CalendarIcon } from './src/icons/CalendarIcon';
+export { default as ChevronIconBlack } from './src/icons/ChevronIconBlack';
+export { default as ChevronIconBlue } from './src/icons/ChevronIconBlue';
+export { default as ChevronIconGray } from './src/icons/ChevronIconGray';
+export { default as ChildIcon } from './src/icons/ChildIcon';
+export { default as DocumentIcon } from './src/icons/DocumentIcon';
+export { default as EditedBySaksbehandlerIcon } from './src/icons/EditedBySaksbehandlerIcon';
+export { default as EditIcon } from './src/icons/EditIcon';
+export { default as ExclamationMarkIcon } from './src/icons/ExclamationMarkIcon';
+export { default as GreenCheckIcon } from './src/icons/GreenCheckIcon';
+export { default as GreenCheckIconFilled } from './src/icons/GreenCheckIconFilled';
+export { default as InfoIcon } from './src/icons/InfoIcon';
+export { default as InstitutionIcon } from './src/icons/InstitutionIcon';
+export { default as OnePersonIcon } from './src/icons/OnePersonIcon';
+export { default as OnePersonIconBlue } from './src/icons/OnePersonIconBlue';
+export { default as OnePersonIconGray } from './src/icons/OnePersonIconGray';
+export { default as OnePersonOutline } from './src/icons/OnePersonOutline';
+export { default as OnePersonOutlineGray } from './src/icons/OnePersonOutlineGray';
+export { default as PlusIcon } from './src/icons/PlusIcon';
+export { default as RedCrossIcon } from './src/icons/RedCrossIcon';
+export { default as RedCrossIconFilled } from './src/icons/RedCrossIconFilled';
+export { default as TwoPersonsIcon } from './src/icons/TwoPersonsIcon';
+export { default as TwoPersonsWithOneHighlightedIconBlue } from './src/icons/TwoPersonsWithOneHighlightedIconBlue';
+export { default as TwoPersonsWithOneHighlightedIconGray } from './src/icons/TwoPersonsWithOneHighlightedIconGray';
+export { default as WarningIcon } from './src/icons/WarningIcon';
