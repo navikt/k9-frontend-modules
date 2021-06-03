0ed776800f5af1e020f30d4df3e6cce8f0716507
Hallvard Andreas Stark <hallvard@scel.to>
2021-06-03 15:33:51 +0200

    la inn ikoner fra medisinsk-vilkår

---
+++ packages/ui/react-components/src/icons/EditedBySaksbehandlerIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const EditedBySaksbehandlerIcon = (): JSX.Element => (
+    <svg
+        className="editedBySaksbehandlerIcon"
+        width="44"
+        height="55"
+        viewBox="0 0 44 55"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M27.9584 11.4583C27.9584 17.7866 22.8283 22.9167 16.5 22.9167C10.1718 22.9167 5.04171 17.7866 5.04171 11.4583C5.04171 5.13007 10.1718 0 16.5 0C22.8283 0 27.9584 5.13007 27.9584 11.4583ZM16.5 25.2083C7.64047 25.2083 0.458374 32.3904 0.458374 41.25H22.5246L30.4503 33.3244C27.6904 28.4771 22.477 25.2083 16.5 25.2083ZM42.3187 36.0564C40.0768 33.8145 36.442 33.8145 34.2001 36.0564L24.423 45.8333L23.3752 55L32.5417 53.952L42.3187 44.1751C44.5607 41.9332 44.5607 38.2983 42.3187 36.0564Z"
+            fill="#FF9100"
+        />
+    </svg>
+);
+
+export default EditedBySaksbehandlerIcon;
