2abe8947a8aa955eeb4886e91430ae4df5ed8468
Hallvard Andreas Stark <hallvard@scel.to>
2021-06-09 13:43:54 +0200

    rettet feil i path

--- packages/util/form-utils/index.ts
+++ packages/util/form-utils/index.ts
@@ -1,6 +1,6 @@
-export { default as CheckboxGroup } from './src/form/wrappers/CheckboxGroup';
-export { default as Datepicker } from './src/form/wrappers/Datepicker';
-export { default as PeriodpickerList } from './src/form/wrappers/PeriodpickerList';
-export { default as RadioGroupPanel } from './src/form/wrappers/RadioGroupPanel';
-export { default as TextArea } from './src/form/wrappers/TextArea';
-export { default as YesOrNoQuestion } from './src/form/wrappers/YesOrNoQuestion';
+export { default as CheckboxGroup } from './src/wrappers/CheckboxGroup';
+export { default as Datepicker } from './src/wrappers/Datepicker';
+export { default as PeriodpickerList } from './src/wrappers/PeriodpickerList';
+export { default as RadioGroupPanel } from './src/wrappers/RadioGroupPanel';
+export { default as TextArea } from './src/wrappers/TextArea';
+export { default as YesOrNoQuestion } from './src/wrappers/YesOrNoQuestion';
