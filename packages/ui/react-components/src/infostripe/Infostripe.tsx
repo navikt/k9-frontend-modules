5c3e6779a1625300251035d3a46c26982742f972
vebnor <vebjorn.nordby@hotmail.com>
2021-08-23 14:52:52 +0200

    gjør det mulig å sende inn et element i infostripe

--- packages/ui/react-components/src/infostripe/Infostripe.tsx
+++ packages/ui/react-components/src/infostripe/Infostripe.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './infostripe.less';
 
-interface InfostripeProps {
-    text: string;
+interface BaseInfostripeProps {
     iconRenderer: () => React.ReactNode;
 }
 
-const Infostripe = ({ text, iconRenderer }: InfostripeProps) => (
+interface ElementInfostripeProps extends BaseInfostripeProps {
+    element: JSX.Element;
+    text?: string;
+}
+interface TextInfostripeProps extends BaseInfostripeProps {
+    text: string;
+    element?: JSX.Element;
+}
+type InfostripeProps = ElementInfostripeProps | TextInfostripeProps;
+const Infostripe = ({ text, iconRenderer, element }: InfostripeProps) => (
     <div className="infostripe">
         <div className="infostripe__iconContainer">{iconRenderer()}</div>
         <div className="infostripe__textContainer">
-            <p className="infostripe__text">{text}</p>
+            {element ? element : <p className="infostripe__text">{text}</p>}
         </div>
     </div>
 );
