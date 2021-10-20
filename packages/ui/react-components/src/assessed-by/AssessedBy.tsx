81ddd93e7a363a1ebbcc964326c30b3f8d0b3140
vebnor <vebjorn.nordby@nav.no>
2021-10-20 09:16:32 +0200

    fjerner kommentar

--- packages/ui/react-components/src/assessed-by/AssessedBy.tsx
+++ packages/ui/react-components/src/assessed-by/AssessedBy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CaseworkerFilled } from '@navikt/ds-icons';
-import {prettifyDateString} from '@navikt/k9-date-utils'
+import { prettifyDateString } from '@navikt/k9-date-utils';
 import './assessedBy.less';
 
 interface IAssessedByProps {
@@ -13,7 +13,6 @@ export default function AssessedBy({ ident, dato }: IAssessedByProps) {
     if (!ident) {
         return null;
     }
-    //2021-10-19T13:23:44.995
     return (
         <div className={'assessed-by'}>
             <CaseworkerFilled height="1.5em" width="1.5em" />
