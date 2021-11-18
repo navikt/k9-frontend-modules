56b8c4d01fd59caf247f852e397d57b54808c2e1
vebnor <vebjorn.nordby@nav.no>
2021-11-18 13:13:04 +0100

    sender inn navn i stedet for ident

--- packages/ui/react-components/src/assessed-by/AssessedBy.tsx
+++ packages/ui/react-components/src/assessed-by/AssessedBy.tsx
@@ -4,19 +4,18 @@ import { prettifyDateString } from '@navikt/k9-date-utils';
 import './assessedBy.less';
 
 interface IAssessedByProps {
-    ident?: string;
-    dato?: string;
+    name?: string;
+    date?: string;
 }
 
-export default function AssessedBy({ ident, dato }: IAssessedByProps) {
-    const navn = undefined;
-    if (!ident) {
+export default function AssessedBy({ name, date }: IAssessedByProps) {
+    if (!name) {
         return null;
     }
     return (
         <div className={'assessed-by'}>
             <CaseworkerFilled height="1.5em" width="1.5em" />
-            <span>{`Vurdering av ${navn ? navn : ident}${dato ? `, ${prettifyDateString(dato)}` : ''}`}</span>
+            <span>{`Vurdering av ${name ? name : ''}${date ? `, ${prettifyDateString(date)}` : ''}`}</span>
         </div>
     );
 }
