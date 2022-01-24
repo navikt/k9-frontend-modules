7fd1127350920f58a80752c7ae7781b3813f4886
eras-dev <emma.rasmusson@nav.no>
2022-01-24 06:42:27 +0100

    mockdata for stories

---
+++ packages/ui/react-components/.storybook/mocked-data/dokument.ts
@@ -0,0 +1,20 @@
+export const dokumenter = [
+  {
+    id: "2",
+    navn: "Dokument 2",
+    type: "MEDISINSKE_OPPLYSNINGER",
+    datert: "2022-01-21",
+    links: [],
+    benyttet: true,
+    annenPartErKilde: false,
+    fremhevet: false,
+    behandlet: true,
+    mottattDato: "2021-03-05",
+    mottattTidspunkt: "2021-03-05T10:23:13.309267",
+    duplikater: [
+      "3"
+    ],
+    duplikatAvId: null,
+    bruktTilMinstEnVurdering: false
+  }
+];
